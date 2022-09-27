45011ba141c5fc951d60ec2773fcdd16068adac2
Yasaswini-Juvvala <112235169+Yasaswini-Juvvala@users.noreply.github.com>
2022-09-27 13:28:58 +0530

    Add files via upload

---
+++ PRINT PATTERN (7).cpp
@@ -0,0 +1,21 @@
+#include<iostream> 
+using namespace std; int main() 
+{  	int i,j,n; 
+ 	cout<<"enter the number of rows => ";
+	   	cin>>n; 
+	 	for(i=0;i<n+1;i++) 
+	 	{ 
+    for(j=0;j<i+1;j++)     
+	  1
+	  	1 	 	 	 
+    1    2
+		1 	 	 
+  1    3    31    4    6    4    1 
+  	1 	 
+
+	 	 	{ 
+	 	 	 	cout<<"*"; 
+	 	 	} 
+	 	 	cout<<"\n"; 
+	 	} 
+} 
